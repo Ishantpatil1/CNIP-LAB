163bc45e30df6462d6bbea7af0c36e4faed83dce
ISHANT PATIL <165829237+Ishantpatil1@users.noreply.github.com>
2024-11-24 14:30:45 +0530

    Create Introduction to wireshark.cpp

---
+++ Introduction to wireshark.cpp
@@ -0,0 +1,8 @@
+Install Wireshark: Download and install from wireshark.org.
+Start Wireshark: Open it and select your active network interface (e.g., Ethernet or Wi-Fi).
+Begin Capture: Click Start to capture packets.
+Generate Traffic: Open a browser and visit http://gaia.cs.umass.edu/wireshark-labs/INTRO-wireshark-file1.html.
+Stop Capture: Go back to Wireshark and click Stop.
+Filter HTTP Packets: Type http in the filter bar to show only HTTP traffic.
+Analyze Packets: Find the HTTP GET request to the URL. Expand the HTTP section to view details.
+Finish: Close Wireshark.
